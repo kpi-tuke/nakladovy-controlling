cec9d0814eaeccf8d966ceb7e43414cc1f50995b
Filip Katušin <--global>
2024-07-22 19:28:36 +0200

    fixed pdf table

--- src/renderer/components/tables/PDFTable.tsx
+++ src/renderer/components/tables/PDFTable.tsx
@@ -2,7 +2,7 @@ import { splitTable } from '../../helper';
 import { RootState } from '../../store/store';
 import { defaultState } from '../../store/rootReducer';
 import { useAppSelector } from '../../store/hooks';
-import { Grid, Paper, styled } from '@mui/material';
+import { Paper, styled, Typography } from '@mui/material';
 import {
   Table,
   TableBody,
@@ -11,12 +11,7 @@ import {
   TableRow,
   TableHead,
 } from './Table';
-import HeaderValue from './HeaderValue';
-
-const TableCellStyled = styled(TableCell)`
-  padding: 0 9px;
-  text-align: left;
-`;
+import React from 'react';
 
 export default function PDFTable({
   selector,
@@ -31,61 +26,95 @@ export default function PDFTable({
     data
   );
 
+  const mergedData = React.useMemo(() => {
+    const tables = [];
+
+    for (
+      let headersIndex = 0;
+      headersIndex < separatedHeaders.length;
+      headersIndex++
+    ) {
+      const headers = separatedHeaders[headersIndex];
+      const data = separatedData[headersIndex];
+
+      tables.push({ headers, data });
+    }
+
+    return tables;
+  }, []);
+
   return (
     <Paper className="hideInScreen">
-      <Grid container>
-        <Grid item xs={4}>
-          {separatedHeaders.map((_table: string[], idx: number) => (
-            <Table key={idx + 'input'}>
-              <TableHead>
-                <TableRow>
-                  <TableCorner>{corner}</TableCorner>
-                </TableRow>
-              </TableHead>
+      {mergedData.map((table, index) => (
+        <div className="new-page-after">
+          <Table
+            key={index}
+            sx={{
+              width: 'unset',
+            }}
+          >
+            <TableHead>
+              <TableRow>
+                <TableCorner
+                  sx={{
+                    minWidth: '30vw',
+                    maxWidth: '30vw',
+                  }}
+                >
+                  {corner}
+                </TableCorner>
 
-              <TableBody>
-                {items.map((value: string, row: number) => {
-                  return !!value ? (
-                    <TableRow key={row}>
-                      <TableCellStyled key={value + row.toString()}>
+                {table.headers.map((header, headerIndex) => (
+                  <TableCell
+                    key={headerIndex}
+                    sx={{
+                      minWidth: '13.3vw',
+                      maxWidth: '13.3vw',
+                    }}
+                  >
+                    {header}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {table.data.map((rowData, rowIndex) =>
+                !!items[rowIndex] ? (
+                  <TableRow key={rowIndex}>
+                    <TableCell
+                      sx={{
+                        textAlign: 'left',
+                        padding: '0 9px',
+                      }}
+                    >
+                      {items[rowIndex]}
+                    </TableCell>
+                    {rowData.map((value, colIndex) => (
+                      <TableCell
+                        key={colIndex}
+                        sx={{
+                          minWidth: '13.3vw',
+                          maxWidth: '13.3vw',
+                        }}
+                      >
                         {value}
-                      </TableCellStyled>
-                    </TableRow>
-                  ) : (
-                    <></>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          ))}
-        </Grid>
-
-        <Grid item xs={8}>
-          {separatedData.map((table: number[][], idx: number) => (
-            <Table
-              key={idx + 'data'}
-              sx={{
-                width: 'unset',
-              }}
-            >
-              <HeaderValue header={separatedHeaders[idx]} />
-              <TableBody>
-                {table.map((rowData: number[], row: number) =>
-                  row < items.length - 1 ? (
-                    <TableRow key={row}>
-                      {rowData.map((value: number, col: number) => (
-                        <TableCell key={row + ':' + col}>{value}</TableCell>
-                      ))}
-                    </TableRow>
-                  ) : (
-                    <></>
-                  )
-                )}
-              </TableBody>
-            </Table>
-          ))}
-        </Grid>
-      </Grid>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ) : (
+                  <></>
+                )
+              )}
+            </TableBody>
+          </Table>
+          {mergedData.length > 1 && (
+            <Typography textAlign={'right'} variant="caption">
+              {index + 1} / {mergedData.length}
+            </Typography>
+          )}
+        </div>
+      ))}
     </Paper>
   );
 }
