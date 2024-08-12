61e47c63ff7cfcf88c1274ceedab054dd2c2ca19
Filip Katušin <--global>
2024-08-12 17:31:32 +0200

    fixed textarea background in dark mode

--- src/renderer/components/Textarea.tsx
+++ src/renderer/components/Textarea.tsx
@@ -1,5 +1,10 @@
-import { debounce, TextareaAutosize } from '@mui/material';
+import {
+  debounce,
+  TextareaAutosize,
+  useTheme as useMuiTheme,
+} from '@mui/material';
 import SectionTitle from './SectionTitle';
+import { useTheme } from './providers/ThemeProvider';
 
 type Props = {
   onChangeDebounced?: (value: string) => void;
@@ -10,6 +15,9 @@ const Textarea: React.FC<Props> = ({
   defaultValue,
   onChangeDebounced: onChange,
 }) => {
+  const theme = useMuiTheme();
+  const { mode } = useTheme();
+
   const debouncedOnChange = (value: string) => {
     onChange(value);
   };
@@ -29,12 +37,19 @@ const Textarea: React.FC<Props> = ({
         onChange={(e) => {
           handleChange(e);
         }}
+        placeholder="Sem napíšte záver a zhodnotenie analýzy..."
         minRows={6}
         style={{
           fontSize: '16px',
           outline: 'none',
           borderRadius: '8px',
           padding: '8px',
+          backgroundColor: theme.palette.background.paper,
+          ...(mode === 'dark' && {
+            border: 'none',
+            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05))`,
+            color: theme.palette.text.primary,
+          }),
         }}
       />
     </div>
