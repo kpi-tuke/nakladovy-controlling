d2271b85e0e78f980db1e1d791032864a10fd116
Filip Katušin <filip.katusin@student.tuke.sk>
2024-10-23 18:23:30 +0200

    added variation analysis result

--- src/renderer/src/pages/variation/VariationResults.tsx
+++ src/renderer/src/pages/variation/VariationResults.tsx
@@ -1,8 +1,12 @@
-import { Box, styled, TextField } from '@mui/material';
+import { Box, Paper, styled, TextField, Typography } from '@mui/material';
 import SectionTitle from '@renderer/components/SectionTitle';
 import { variationActions, selectors } from './variationSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import Spacer from '@renderer/components/Spacer';
+import TableStatic from '@renderer/components/TableStatic';
+import { variationCalculation } from './variationCalculation';
+import BarGraph from '@renderer/components/graph/BarGraph';
+import { transposeMatrix } from '@renderer/helper';
 
 const InputWrapper = styled(Box)`
   display: flex;
@@ -18,6 +22,13 @@ const YearLabel = styled(SectionTitle)`
 const VariationResults = () => {
   const dispatch = useAppDispatch();
 
+  const items = useAppSelector(selectors.items);
+  const data = useAppSelector(selectors.data);
+
+  const { absolutnaDiferencia, plneniePlanu } = variationCalculation(
+    data as number[][],
+  );
+
   // @ts-ignore
   const additionalData = useAppSelector(selectors.getAdditionalData!) as any;
 
@@ -31,6 +42,9 @@ const VariationResults = () => {
       }),
     );
   };
+
+  console.log('absolutnaDiferencia: ', absolutnaDiferencia);
+  console.log('plneniePlanu: ', plneniePlanu);
 
   return (
     <div>
@@ -52,6 +66,67 @@ const VariationResults = () => {
           type="number"
         />
       </InputWrapper>
+
+      <Spacer height={40} />
+
+      <SectionTitle className="new-page">Analýza ukazovateľov</SectionTitle>
+
+      <div>
+        {items.filter(Boolean).map((item, index) => (
+          <Paper
+            key={index}
+            sx={{
+              '&:not(:last-child)': {
+                marginBottom: '40px',
+              },
+            }}
+          >
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{
+                marginLeft: 2,
+                marginTop: 1,
+                fontSize: '18px',
+              }}
+            >
+              {item}
+            </Typography>
+            <TableStatic
+              corner={'Ekonomické ukazovatele'}
+              header={['']}
+              inputs={[
+                [
+                  '(AD) - absolútna diferencia (€)',
+                  `\\(AD = skutočnosť - plán\\)`,
+                ],
+                [
+                  '(I) plnenia plánu (%)',
+                  `\\(I=\\frac{skutočnosť}{plán} * 100\\% \\)`,
+                ],
+              ]}
+              data={[[absolutnaDiferencia[index]], [plneniePlanu[index]]]}
+              newPageAfter={false}
+            />
+          </Paper>
+        ))}
+      </div>
+
+      <Spacer height={40} />
+
+      <BarGraph
+        title={'Prehľad ekonomických veličin (plán/skutočnosť)'}
+        height={420}
+        labels={items.filter(Boolean)}
+        data={[
+          { name: 'plán', values: transposeMatrix(data)[0] as number[] },
+          {
+            name: 'skutočnosť',
+            values: transposeMatrix(data)[1] as number[],
+          },
+        ]}
+        yAxisLabel="ekonomická veličina (€)"
+      />
     </div>
   );
 };
---
+++ src/renderer/src/pages/variation/variationCalculation.ts
@@ -0,0 +1,25 @@
+import { divideArrays, multiplyArrays, subtractArrays } from '@renderer/helper';
+import { formatNumber } from '@renderer/utils/formatNumber';
+
+export const variationCalculation = (data: number[][]) => {
+  const plan: number[] = [];
+  const reality: number[] = [];
+
+  data.forEach((rowData, idx) => {
+    plan[idx] = formatNumber(rowData[0]);
+
+    reality[idx] = formatNumber(rowData[1]);
+  });
+
+  const absolutnaDiferencia = subtractArrays(reality, plan);
+
+  const plneniePlanu = multiplyArrays(
+    divideArrays(reality, plan),
+    new Array(plan.length).fill(100),
+  );
+
+  return {
+    absolutnaDiferencia,
+    plneniePlanu,
+  };
+};
