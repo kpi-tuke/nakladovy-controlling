f272c22f3e58c3ed716bc72888e7021bf37784c8
Filip Katušin <filip.katusin@student.tuke.sk>
2025-02-12 15:47:45 +0100

    TableData

---
+++ src/renderer/src/__tests__/tables/TableData.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { TableData } from '@renderer/components/tables/TableData';
+import { render } from '@testing-library/react';
+import { matchers } from '@emotion/jest';
+
+expect.extend(matchers);
+
+describe('TableData styled component', () => {
+  it('should have overflow: auto by default', () => {
+    const { container } = render(<TableData>Content</TableData>);
+    expect(container.firstChild).toHaveStyleRule('overflow', 'auto');
+  });
+
+  it('should set overflow to unset in print media query', () => {
+    const { container } = render(<TableData>Content</TableData>);
+    expect(container.firstChild).toHaveStyleRule('overflow', 'unset', {
+      media: 'print',
+    });
+  });
+
+  it('matches the snapshot', () => {
+    const { container } = render(<TableData>Content</TableData>);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
